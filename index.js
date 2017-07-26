287f5af188366a5dff0443d98e76c32079697cb9
Sarath <sarath.hacks@gmail.com>
2017-07-26 12:12:52 +0530

    Added express example

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
 var { graphql, buildSchema } = require('graphql');
 
 var schema = buildSchema(`
@@ -8,8 +10,13 @@ var schema = buildSchema(`
   }
 `);
 
+
 var root = { hello: () => 'Hello world!',name:()=> 'Sarath' };
 
-graphql(schema, '{ hello,name }', root).then((response) => {
-  console.log(response);
-});
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
